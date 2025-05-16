5bf2bfb5fab8ac300c65051b989b90e5468ed292
amirbettermode <amirhossein@bettermode.com>
2025-05-16 12:44:46 +0330

    Update import statement in `server/db/index.ts` to include file extension for schema import

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,7 +1,7 @@
 // server/db/index.ts
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
-import * as schema from './schema';
+import * as schema from './schema.js';
 
 const connectionString = process.env.DATABASE_URL;
 
